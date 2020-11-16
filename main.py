28f437376f6219112ce861b2686187fa10fd8207
Krushay Bhavsar <krushaybhavsar@gmail.com>
2020-11-16 17:18:52 -0500

    File Upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import zipfile
+
+charlist = 'abcdefghijklmnopqrstuvwxyz'
+complete = []
+
+for current in range(4):
+    a = [i for i in charlist]
+    for x in range(current):
+        a = [y + i for i in charlist for y in a]
+    complete = complete + a
+
+z = zipfile.ZipFile('secret.zip')
+
+tries = 0
+
+for password in complete:
+    try:
+        tries += 1
+        z.setpassword(password.encode('ascii'))
+        z.extract('secret.txt')
+        print(f'Password was found fter {tries} tries! It was {password}')
+    except:
+        pass
